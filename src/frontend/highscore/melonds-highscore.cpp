927f3e56e1d42144147864e454c4b4f0cdd6f76f
Alice Mikhaylenko <alicem@gnome.org>
2025-11-05 02:54:55 +0400

    highscore: Stop multiplying volume
    Turns out audio was just clipping, rather than a sample rate issue.

--- src/frontend/highscore/melonds-highscore.cpp
+++ src/frontend/highscore/melonds-highscore.cpp
@@ -18,7 +18,6 @@
 #define SCREEN_HEIGHT 192
 #define SAMPLE_RATE 48000
 #define MAX_SAMPLES 2500
-#define VOLUME_MULTIPLIER 1.5
 #define N_BAD_FRAMES 1
 
 #define USE_GL 0
@@ -459,9 +458,6 @@ melonds_core_run_frame (HsCore *core)
 
   u32 n_samples = self->console->SPU.GetOutputSize ();
   self->console->SPU.ReadOutput (self->audio_buffer, n_samples);
-
-  for (int i = 0; i < n_samples * 2; i++)
-    self->audio_buffer[i] *= VOLUME_MULTIPLIER;
 
   hs_core_play_samples (core, self->audio_buffer, n_samples * 2);
 
